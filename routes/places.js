eb7e0b09e748b3ca75e3eda71008129287136e9a
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-02-01 20:19:06 +0100

    working on location feature

--- routes/places.js
+++ routes/places.js
@@ -25,12 +25,74 @@ router.post('/create', (req, res, next) => {
     });
 });
 
+router.get('/search', (req, res, next) => {
+  Place.find()
+    .then((places) => {
+      res.render('place/results', { places: places });
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
+
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
   Place.findById(id)
     .then((place) => {
       console.log('whyyyyyy');
       res.render('place/single', { place });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get('/:id/update', (req, res, next) => {
+  const id = req.params.id;
+  Place.findById(id)
+    .then((place) => {
+      res.render('place/update', { place: place });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post('/:id/update', (req, res, next) => {
+  const id = req.params.id;
+  const data = req.body;
+  Place.findByIdAndUpdate(id, {
+    name: data.name,
+    type: data.type,
+    location: {
+      coordinates: [data.longitude, data.latitude]
+    }
+  })
+    .then((place) => {
+      res.redirect(`/place/${place._id}`);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get('/:id/delete', (req, res, next) => {
+  const id = req.params.id;
+  Place.findById(id)
+    .then((place) => {
+      res.render('place/confirm-deletion', { place: place });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post('/:id/delete', (req, res, next) => {
+  const id = req.params.id;
+  Place.findByIdAndDelete(id)
+    .then((place) => {
+      res.redirect('/');
     })
     .catch((error) => {
       next(error);
